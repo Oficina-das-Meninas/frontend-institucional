0fb21cb18714815bc70b4f8ef46191a5407eeb49
igorcardosoy <igofilipicardoso@gmail.com>
2025-09-05 03:01:41 -0300

    feat: usando o dropdown e ajustando os estilos para a responsividade

--- src/app/shared/components/toolbar/toolbar.ts
+++ src/app/shared/components/toolbar/toolbar.ts
@@ -1,12 +1,14 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSelectModule } from '@angular/material/select';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
+import { DropdownComponent } from '../dropdown/dropdown';
 
 @Component({
   selector: 'app-toolbar',
@@ -20,20 +22,23 @@ import { RouterLink } from '@angular/router';
     MatSidenavModule,
     MatFormFieldModule,
     MatSelectModule,
+    MatExpansionModule,
+    DropdownComponent,
   ],
   templateUrl: './toolbar.html',
   styleUrl: './toolbar.scss',
 })
 export class Toolbar {
-  showFiller = false;
-
   @ViewChild('drawerContainer', { static: false, read: ElementRef }) drawerContainerRef?: ElementRef;
+  @ViewChild('drawer') drawer!: MatSidenav;
 
-  toggleDrawer(drawer: any): void {
-    if (!drawer.opened && this.drawerContainerRef && this.drawerContainerRef.nativeElement) {
-      this.drawerContainerRef.nativeElement.style.setProperty('z-index', '51', 'important');
+  toggleDrawer(): void {
+    if (this.drawer) {
+      if (!this.drawer.opened && this.drawerContainerRef && this.drawerContainerRef.nativeElement) {
+        this.drawerContainerRef.nativeElement.style.setProperty('z-index', '51', 'important');
+      }
+      this.drawer.toggle();
     }
-    drawer.toggle();
   }
 
   onDrawerClosed(): void {
