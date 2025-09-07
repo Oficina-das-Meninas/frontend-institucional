6d55551344d8d325130be00413c1c2703c325e53
igorcardosoy <igofilipicardoso@gmail.com>
2025-09-07 03:43:04 -0300

    feat: adicionando links, hover e componente da logo, em svg.

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { Logo } from '../logo/logo';
 import { FacebookIconComponent } from '../social-icons/facebook-icon';
 import { InstagramIconComponent } from '../social-icons/instagram-icon';
 import { YoutubeIconComponent } from '../social-icons/youtube-icon';
 
 @Component({
   selector: 'app-footer',
-  imports: [FacebookIconComponent, InstagramIconComponent, YoutubeIconComponent, MatIconModule],
+  imports: [FacebookIconComponent, InstagramIconComponent, YoutubeIconComponent, MatIconModule, RouterLink, Logo],
   standalone: true,
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
