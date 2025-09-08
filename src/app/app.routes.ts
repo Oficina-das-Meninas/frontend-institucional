15edfaf97957e927f493127710ab2d8fb7840a7c
natansalvadorligabo <salvadorligabo@gmail.com>
2025-09-08 14:19:02 -0300

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  {
+    path: 'inicio',
+    loadComponent: () =>
+      import('./domain/home/containers/home/home').then((m) => m.Home),
+  },
+  { path: '**', redirectTo: '/' },
+];
