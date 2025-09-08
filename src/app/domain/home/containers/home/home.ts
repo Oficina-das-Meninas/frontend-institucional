d19f2fe335b13346443145a2fc3ddf507d93a2de
natansalvadorligabo <salvadorligabo@gmail.com>
2025-09-08 14:19:21 -0300

    feat: add home page

---
+++ src/app/domain/home/containers/home/home.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { FlowerStat } from "../../components/flower-stat/flower-stat";
+import { SupportCard } from "../../components/support-card/support-card";
+
+@Component({
+  selector: 'app-home',
+  imports: [MatButtonModule, MatCardModule, FlowerStat, SupportCard],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {
+  yearOfFoundation = new Date('03-23-2002').getFullYear();
+  today = new Date().getFullYear();
+}
