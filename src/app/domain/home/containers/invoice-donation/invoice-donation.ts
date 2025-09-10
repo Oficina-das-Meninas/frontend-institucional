acb1d96a6a99baf440fbbdc7954a8e0fef0a096a
igorcardosoy <igofilipicardoso@gmail.com>
2025-09-10 15:43:26 -0300

    feat: página de doação com responsividade

--- src/app/domain/home/containers/invoice-donation/invoice-donation.ts
+++ src/app/domain/home/containers/invoice-donation/invoice-donation.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-invoice-donation',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './invoice-donation.html',
-  styleUrl: './invoice-donation.scss'
+  styleUrl: './invoice-donation.scss',
 })
-export class InvoiceDonation {
-
-}
+export class InvoiceDonation {}
