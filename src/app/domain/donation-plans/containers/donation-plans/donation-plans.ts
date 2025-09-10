f5cfe53ce6893f0bd84dc37cc7641a8de923fd3e
giovanatrevizanbarbosa <gi.trevizan.barbosa@gmail.com>
2025-09-10 19:34:50 -0300

    feat: add donation plans component with HTML and SCSS

---
+++ src/app/domain/donation-plans/containers/donation-plans/donation-plans.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-donation-plans',
+  imports: [MatIconModule],
+  templateUrl: './donation-plans.html',
+  styleUrl: './donation-plans.scss'
+})
+export class DonationPlans {
+
+}
