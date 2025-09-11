87cb8cc5fdefa1b01721b0273dcf03fc67fa1742
igorcardosoy-fedora <igorfilipicardoso@gmail.com>
2025-09-10 22:54:53 -0300

    feat: esqueleto para o video

--- src/app/domain/home/containers/invoice-donation/invoice-donation.ts
+++ src/app/domain/home/containers/invoice-donation/invoice-donation.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-invoice-donation',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, CommonModule],
   templateUrl: './invoice-donation.html',
   styleUrl: './invoice-donation.scss',
 })
-export class InvoiceDonation {}
+export class InvoiceDonation implements OnInit {
+  isVideoLoading = true;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.isVideoLoading = false;
+    }, 3000);
+  }
+
+  onIframeLoad() {
+    this.isVideoLoading = false;
+  }
+}
