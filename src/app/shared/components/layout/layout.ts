8e0a1813b58be9a24b0ba110eb924d73d1eff3a0
natansalvadorligabo <salvadorligabo@gmail.com>
2025-09-11 21:16:17 -0300

    fix: remove mobile query handling

--- src/app/shared/components/layout/layout.ts
+++ src/app/shared/components/layout/layout.ts
@@ -11,22 +11,5 @@ import { Toolbar } from './toolbar/toolbar';
   styleUrl: './layout.scss',
 })
 export class Layout {
-  protected readonly isMobile = signal(true);
-
-  private readonly _mobileQuery: MediaQueryList;
-  private readonly _mobileQueryListener: () => void;
-
-  constructor() {
-    const media = inject(MediaMatcher);
-
-    this._mobileQuery = media.matchMedia('(max-width: 600px)');
-    this.isMobile.set(this._mobileQuery.matches);
-    this._mobileQueryListener = () =>
-      this.isMobile.set(this._mobileQuery.matches);
-    this._mobileQuery.addEventListener('change', this._mobileQueryListener);
-  }
-
-  ngOnDestroy() {
-    this._mobileQuery.removeEventListener('change', this._mobileQueryListener);
-  }
+  
 }
