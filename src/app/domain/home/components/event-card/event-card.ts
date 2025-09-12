fc488a6ef85b37b2bfdc8574bc367f4816fc6b16
igorcardosoy <igofilipicardoso@gmail.com>
2025-09-12 16:41:24 -0300

    refactor: utilizando DatePipe para a formatação da data

--- src/app/domain/home/components/event-card/event-card.ts
+++ src/app/domain/home/components/event-card/event-card.ts
@@ -1,8 +1,9 @@
+import { DatePipe, UpperCasePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-event-card',
-  imports: [],
+  imports: [DatePipe, UpperCasePipe],
   templateUrl: './event-card.html',
   styleUrl: './event-card.scss',
 })
@@ -11,8 +12,4 @@ export class EventCard {
   @Input() description: string = 'Descrição do Evento';
   @Input() imageUrl: string = 'https://picsum.photos/400';
   @Input() date: Date = new Date();
-  formattedDate: string = this.date
-    .toLocaleDateString('pt-BR', { day: '2-digit', month: 'short', year: 'numeric' })
-    .replace(' de ', ' ')
-    .toUpperCase();
 }
