f7720679b0ea154fff346ce210944bd75d77c061
LucasAlt40 <lucas.alt40@gmail.com>
2025-09-17 19:26:55 -0300

    feat: iniciado desenvolvimento da tela de doação

---
+++ src/app/domain/donation/containers/make-your-donation/make-your-donation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-make-your-donation',
+  imports: [],
+  templateUrl: './make-your-donation.html',
+  styleUrl: './make-your-donation.scss'
+})
+export class MakeYourDonation {
+
+}
