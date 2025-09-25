ddbada1e16179bd5970ceb5ecb213e743b94553a
natansalvadorligabo <salvadorligabo@gmail.com>
2025-09-25 11:11:17 -0300

    feat: implement calendar filter component with date range selection

---
+++ src/app/shared/components/calendar-filter/calendar-filter.ts
@@ -0,0 +1,61 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MAT_DATE_LOCALE,
+  provideNativeDateAdapter,
+} from '@angular/material/core';
+import { MatDatepickerIntl, MatDatepickerModule } from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { getPortugueseDatepickerIntl } from '../../../i18n/pt-BR';
+import { DateRange } from '../../models/date-range';
+
+@Component({
+  selector: 'app-calendar-filter',
+  imports: [
+    FormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatDatepickerModule,
+    MatBadgeModule
+  ],
+  providers: [
+    provideNativeDateAdapter(),
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
+    { provide: MatDatepickerIntl, useFactory: getPortugueseDatepickerIntl },
+  ],
+  templateUrl: './calendar-filter.html',
+  styleUrl: './calendar-filter.scss',
+})
+export class CalendarFilter {
+  @Input() dateRange: DateRange = { start: null, end: null };
+  @Input() tooltipText = 'Filtrar por data';
+
+  @Output() dateRangeChange = new EventEmitter<DateRange>();
+  @Output() apply = new EventEmitter<DateRange>();
+  @Output() clear = new EventEmitter<void>();
+
+  onClearDateFilter(): void {
+    this.dateRange = { start: null, end: null };
+    this.dateRangeChange.emit(this.dateRange);
+    this.clear.emit();
+  }
+
+  onApplyDateFilter(): void {
+    this.dateRangeChange.emit(this.dateRange);
+    this.apply.emit(this.dateRange);
+  }
+
+  onDateRangeChanged(): void {
+    this.dateRangeChange.emit(this.dateRange);
+  }
+
+  hasDateFilter(): boolean {
+    return !!(this.dateRange.start || this.dateRange.end);
+  }
+}
