12f65cd011eaf9853c00ff0ef29b6ed02ced8eca
igorcardosoy-fedora <igorfilipicardoso@gmail.com>
2025-09-26 22:04:40 -0300

    fix: update bucketUrl to use secure HTTPS protocol

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api-dev.apollomusic.com.br/api',
-  bucketUrl: 'http://35.247.193.159:9000/oficinadasmeninasteste/pub'
+  bucketUrl: 'https://files.apollomusic.com.br/oficinadasmeninasteste/pub'
 };
