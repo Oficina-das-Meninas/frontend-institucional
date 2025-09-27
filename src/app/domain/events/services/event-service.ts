782eda1f087df3c1926ef85855dbe8ace494976a
igorcardosoy <igofilipicardoso@gmail.com>
2025-09-27 17:06:39 -0300

    feat: implementando um cache simples para enventos e adicionando o link da imagem aos eventos

--- src/app/domain/events/services/event-service.ts
+++ src/app/domain/events/services/event-service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { first, Observable } from 'rxjs';
+import { first, map, Observable, of } from 'rxjs';
 import { environment } from '../../../../environments/environment.development';
+import { ImageService } from '../../../shared/services/image-service';
 import { toLocalDateTime } from '../../../shared/utils/date-utils';
 import { Event } from '../model/event';
 import { EventPage } from '../model/event-page';
@@ -13,9 +14,29 @@ import { EventPage } from '../model/event-page';
 export class EventService {
   private readonly API_URL = `${environment.apiUrl}/events`;
   private httpClient = inject(HttpClient);
+  private imageService = inject(ImageService);
+
+  private eventCache = new Map<string, Event>();
+
+  private transformEvent(event: Event): Event {
+    return {
+      ...event,
+      previewImageUrl: this.imageService.getImageUrl(event.previewImageUrl),
+      partnersImageUrl: event.partnersImageUrl ? this.imageService.getImageUrl(event.partnersImageUrl) : undefined
+    } as Event;
+  }
 
   getById(id: string): Observable<Event> {
-    return this.httpClient.get<Event>(`${this.API_URL}/${id}`, {}).pipe(first());
+    if (this.eventCache.has(id)) return of(this.eventCache.get(id)!);
+
+    return this.httpClient.get<Event>(`${this.API_URL}/${id}`, {}).pipe(
+      first(),
+      map(event => {
+        const transformedEvent = this.transformEvent(event);
+        this.eventCache.set(id, transformedEvent);
+        return transformedEvent;
+      })
+    );
   }
 
   list(
@@ -40,6 +61,19 @@ export class EventService {
 
     return this.httpClient
       .get<EventPage>(this.API_URL, { params })
-      .pipe(first());
+      .pipe(
+        first(),
+        map(eventPage => {
+          const transformed = {
+            ...eventPage,
+            data: eventPage.data.map(event => {
+              const transformedEvent = this.transformEvent(event);
+              this.eventCache.set(event.id, transformedEvent);
+              return transformedEvent;
+            })
+          };
+          return transformed as EventPage;
+        })
+      );
   }
 }
