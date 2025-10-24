82a036aac7da4fe389f5a59b5b9010a5711b1e42
LucasAlt40 <lucas.alt40@gmail.com>
2025-10-24 19:32:13 -0300

    feat: criado enviroment local para desenvolvimento

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'https://api-dev.apollomusic.com.br/api',
-  bucketUrl: 'https://files.apollomusic.com.br/oficinadasmeninasteste'
+  bucketUrl: 'https://files.apollomusic.com.br/oficinadasmeninasteste',
+  captchaSiteKey: '6LdbC-grAAAAANM6iLnFAGJyJE1aOgq8Ba83gEeZ',
 };
