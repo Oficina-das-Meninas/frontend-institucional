7d9ff17680b090907b422863f226a72a383a2246
Igor Cardoso <66184416+igorcardosoy@users.noreply.github.com>
2025-10-31 22:09:31 -0300

    113 refatoracao dos parceiros na pagina home (#115)
    * feat: add title option to carousel and update image obaject to contain
    
    * feat: enable title display in carousel and integrate MatTooltip
    
    * refactor: remove MatTooltip import from carousel component
    
    * feat: implement loading state in event card and update template for click handling
    
    * fix: remove unnecessary shadow from partners image in event details
    
    * fix: faltando chaves
    
    * fix: correções do copilot

--- src/app/domain/events/components/event-card/event-card.ts
+++ src/app/domain/events/components/event-card/event-card.ts
@@ -1,6 +1,7 @@
 import { DatePipe, registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
-import { Component, Input, LOCALE_ID } from '@angular/core';
+import { Component, Input, LOCALE_ID, OnDestroy, signal } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterLink } from '@angular/router';
 
@@ -8,19 +9,29 @@ registerLocaleData(localePt);
 
 @Component({
   selector: 'app-event-card',
-  imports: [DatePipe, MatTooltipModule, RouterLink],
+  imports: [DatePipe, MatTooltipModule, RouterLink, MatProgressSpinnerModule],
   templateUrl: './event-card.html',
   styleUrl: './event-card.scss',
   providers: [
     { provide: LOCALE_ID, useValue: 'pt-br' }
   ]
 })
-export class EventCard {
+export class EventCard implements OnDestroy {
   @Input() eventId!: string;
   @Input() eventTitle!: string;
   @Input() description!: string;
   @Input() imageUrl!: string;
   @Input() date!: Date;
+
+  isLoading = signal(false);
+
+  onCardClick() {
+    this.isLoading.set(true);
+  }
+
+  ngOnDestroy() {
+    this.isLoading.set(false);
+  }
 
   getTooltipText(): string {
     return this.description.replace(/<[^>]*>/g, '').trim();
