ea64c2e073e2f444edccb0a4f91a3f4503c73f35
Igor Cardoso <66184416+igorcardosoy@users.noreply.github.com>
2025-11-23 18:37:06 -0300

    Update apiUrl for development environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://api-dev.oficinadasmeninas.com.br/api',
+  apiUrl: 'http://api-dev.oficinadasmeninas.org.br/api',
   bucketUrl: 'http://files.oficinadasmeninas.org.br/oficinadasmeninasteste',
   captchaSiteKey: '6LdbC-grAAAAANM6iLnFAGJyJE1aOgq8Ba83gEeZ',
 };
