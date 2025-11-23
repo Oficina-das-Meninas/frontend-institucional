048acc3d6a8caeb2aeb2a0de2b68c00076e30744
Igor Cardoso <66184416+igorcardosoy@users.noreply.github.com>
2025-11-23 20:19:42 -0300

    Change API and bucket URLs to HTTPS
    Updated API and bucket URLs to use HTTPS.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://api-dev.oficinadasmeninas.org.br/api',
-  bucketUrl: 'http://files.oficinadasmeninas.org.br/oficinadasmeninasteste',
+  apiUrl: 'https://api-dev.oficinadasmeninas.org.br/api',
+  bucketUrl: 'https://files.oficinadasmeninas.org.br/oficinadasmeninasteste',
   captchaSiteKey: '6LdbC-grAAAAANM6iLnFAGJyJE1aOgq8Ba83gEeZ',
 };
